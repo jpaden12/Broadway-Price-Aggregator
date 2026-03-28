d27cca042d75151d40b0eb86a9bf41282b3b51c8
Paden, Jamal R <jpaden@gatech.edu>
2026-03-28 17:27:44 -0400

    Adding client linter settings

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -2,10 +2,33 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { 
+      js, 
+      react 
+    },
+    extends: ["js/recommended"],
+    languageOptions: 
+    { 
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        defaultProject: ['tsconfig.json'],
+        sourceType: "module"
+      }
+    },
+    settings: {
+      react: { version: "19" }
+    }
+  },
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  [globalIgnores(["*.config.ts", "*.config.js", "*.config.mjs"])]
 ]);
